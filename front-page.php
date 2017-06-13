16cef7cbe1b1ffbd9a8c6d8aa40259a9557c2b90
Kei Kreutler <kei@ourmachine.net>
2017-06-14 00:44:44 +0300

    Added all content.

--- front-page.php
+++ front-page.php
@@ -5,13 +5,13 @@
 		<a href="#alloys">
 			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-rg.png">
 		</a>
-		<a href="#demo">
+		<a href="#interface">
 			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-rg.png">
 		</a>
-		<a href="#demo">
+		<a href="#economics">
 			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-rg.png">
 		</a>
-		<a href="#demo">
+		<a href="#urban">
 			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-rg.png">
 		</a>
 	</div>
@@ -20,13 +20,13 @@
 		<a href="#alloys">
 			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-sm.png">
 		</a>
-		<a href="#demo">
+		<a href="#interface">
 			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-sm.png">
 		</a>
-		<a href="#demo">
+		<a href="#economics">
 			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-sm.png">
 		</a>
-		<a href="#demo">
+		<a href="#urban">
 			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-sm.png">
 		</a>
 	</div>
@@ -70,7 +70,7 @@
 			</div>
 			<div class="col-lg-5 col-md-5 col-sm-12 col-sm-12">
 				<p>
-				Patternist out of world game description. In publishing and graphic design, lorem ipsum is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. Replacing meaningful content with placeholder text allows designers to design the form of the content before the content itself has been produced.
+				This is a little information about the game for public clarity. Patternist out of world game description. In publishing and graphic design, lorem ipsum is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. Replacing meaningful content with placeholder text allows designers to design the form of the content before the content itself has been produced.
 				</p>
 
 				<p>
@@ -85,23 +85,156 @@
 
 <section class="feature-2">	
 
-
 </section>
 
 
 <a name="alloys">
-	<section>
+	<section id="alloys" class="dark">
 		<div class="container">
 
 			<h1>Create your world.</h1>
 
 			<img src="/wp-content/themes/patternist-theme/images/alloy_matrix.png">
 
+			<p class="center">This is a little information about the alchemical system of the game.</p>
+
 		</div>
 		
 	</section>
 </a>
 
+<section class="feature-3">	
+
+</section>
+
+
+<a name="interface">
+	<section class="blue">
+		<div class="container">
+
+			<div class="row">
+				<div class="col-lg-12">
+					<h1>Trade, craft and transform.</h1>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-sm-1 col-xs-1">
+					<img src="/wp-content/themes/patternist-theme/images/game_UI_trading.png">
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-1 col-xs-1">
+					<img src="/wp-content/themes/patternist-theme/images/game_UI_crafting.png">
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-1 col-xs-1">
+					<img src="/wp-content/themes/patternist-theme/images/game_UI_building.png">
+				</div>
+			</div>
+
+		</div>
+		
+	</section>
+</a>
+
+<section class="feature-5">
+	<div class="container">
+
+		<h1>WebGL Demo</h1>
+
+	</div>
+	
+</section>
+
+<a name="economics">
+	<section>
+		<div class="container">
+
+			<img src="/wp-content/themes/patternist-theme/images/game-network-barter.jpg">
+
+			<h1>Cryptoeconomica&#8212;Network Barter</h1>
+
+		</div>
+		
+	</section>
+</a>
+
+<section class="feature-2">	
+
+</section>
+
+<a name="urban">
+	<section class="medium">
+		<div class="container">
+
+			<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+				<p>
+				The urbanism of Paternist is a Dyson sphere, which is a hypothetical megastructure that completely encompasses a star and captures most or all of its power output. The concept was first described by Olaf Stapledon in his science fiction novel Star Maker (1937), and later popularized by Freeman Dyson in his 1960 paper "Search for Artificial Stellar Sources of Infrared Radiation".[1] Dyson speculated that such structures would be the logical consequence of the escalating energy needs of a technological civilization and would be a necessity for its long-term survival. He proposed that searching for such structures could lead to the detection of advanced, intelligent extraterrestrial life. Different types of Dyson spheres and their energy-harvesting ability would correspond to levels of technological advancement on the Kardashev scale.
+				</p>
+				<p>
+				Since then, other variant designs involving building an artificial structure or series of structures to encompass a star have been proposed in exploratory engineering or described in science fiction under the name "Dyson sphere". These later proposals have not been limited to solar-power stations, with many involving habitation or industrial elements.
+				</p>
+
+			</div>
+			<div class="col-lg-5 col-md-5 col-sm-12 col-sm-12">
+				<img src="/wp-content/themes/patternist-theme/images/axo.png">
+			</div>
+
+
+		</div>
+		
+	</section>
+</a>
+
+<section class="feature-4">	
+
+</section>
+
+<a name="backstory">
+	<section class="purple">
+		<div class="container">
+
+			<h1>Narrative Backstory&#8212;Statement of the Mystery</h1>
+
+		</div>
+		
+	</section>
+</a>
+
+<section class="feature-6">	
+
+</section>
+
+
+<section>
+	<div class="container">
+		<h1>
+			Can You Imagine this landscape?
+		</h1>
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<img src="/wp-content/themes/patternist-theme/images/landscape-sketch-1.png">
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<img src="/wp-content/themes/patternist-theme/images/landscape-sketch-2.png">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="feature-5">	
+	<div class="container">
+
+	<img src="/wp-content/themes/patternist-theme/images/landscape-topology.png">
+</div>
+</section>
+
+	<section class="blue">
+		<div class="container">
+
+			<h1>Game Guide</h1>
+
+		</div>
+		
+	</section>
 
 
 <?php get_footer(); ?>
