fa8d1654196ace3d88f4492549971c0f47dfe300
Kei Kreutler <kei@ourmachine.net>
2017-06-14 19:12:22 +0300

    Fixed for firefox.

--- header.php
+++ header.php
@@ -7,3 +7,60 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<!-- User Interface -->
+<div class="ui">
+	<div class="dials hidden-md hidden-sm hidden-xs">
+		<a href="#alloys">
+			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-rg.png">
+		</a>
+		<a href="#interface">
+			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-rg.png">
+		</a>
+		<a href="#economics">
+			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-rg.png">
+		</a>
+		<a href="#urban">
+			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-rg.png">
+		</a>
+	</div>
+
+	<div class="dials hidden-lg">
+		<a href="#alloys">
+			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-sm.png">
+		</a>
+		<a href="#interface">
+			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-sm.png">
+		</a>
+		<a href="#economics">
+			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-sm.png">
+		</a>
+		<a href="#urban">
+			<img class="dial-ring" src="/wp-content/themes/patternist-theme/images/dial-ring-sm.png">
+		</a>
+	</div>
+
+	<div class="hidden-sm hidden-xs">
+		<img id="ui-top" src="/wp-content/themes/patternist-theme/images/grid_top.png">
+		<img id="ui-right" src="/wp-content/themes/patternist-theme/images/grid_right.png">
+		<img id="ui-left" src="/wp-content/themes/patternist-theme/images/grid_left.png">
+		<img id="ui-toggle" src="/wp-content/themes/patternist-theme/images/grid_toggle.png">
+		<div id="language-toggle">
+				<?php if ( function_exists( 'the_msls' ) ) the_msls(); ?>
+		</div>
+	</div>
+
+	<div class="hidden-lg hidden-md">
+		<img id="ui-top" src="/wp-content/themes/patternist-theme/images/grid_top_sm.png">
+		<img id="ui-right" src="/wp-content/themes/patternist-theme/images/grid_right_sm.png">
+		<img id="ui-left" src="/wp-content/themes/patternist-theme/images/grid_left_sm.png">
+		<img id="ui-toggle" src="/wp-content/themes/patternist-theme/images/grid_toggle_sm.png">
+	</div>
+
+	<div class="hidden-md hidden-sm hidden-xs">
+		<img id="ui-wasd" src="/wp-content/themes/patternist-theme/images/ui_wasd.png">
+	</div>
+	<div class="hidden-lg">
+		<img id="ui-wasd" src="/wp-content/themes/patternist-theme/images/ui_wasd_sm.png">
+	</div>
+</div>
